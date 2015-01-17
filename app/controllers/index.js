415af08c8dafaffe65885af51ad3065175b3cd37
Matt Klitgaard <mklitgaa@1800contacts.com>
2015-01-17 12:23:13 -0700

    add details controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,12 +1,24 @@
 var myCharacters = Alloy.Collections.characters;
 
 var character = Alloy.createModel('characters', {
-characterName : "Test",
-image: "imageTest"	
+characterName : "Hooba",
+image: "imageTestHooba"	
 });
 
 myCharacters.add(character);
 character.save();
+
+function showCharacter(event)
+{
+	var selectedCharacter = event.source;
+	var args = {
+		characterName: selectedCharacter.characterName,
+		image: selectedCharacter.image
+	};
+	
+	var characterview = Alloy.createController("characterdetails",args).getView();
+	characterview.open();
+}
 
 function doClick(e) {
     alert($.label.text);
